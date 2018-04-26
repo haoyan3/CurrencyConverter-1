c7a9a4444f95c0012d516bb2de50599154de81ce
michaelsikora <msikora3@illinois.edu>
2018-04-26 16:28:13 -0500

    hlhkhhhkl

--- app/src/main/java/edu/illinois/cs/cs125/currencyconverter/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/currencyconverter/MainActivity.java
@@ -2,11 +2,27 @@ package edu.illinois.cs.cs125.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
     private Button convert;
     private TextView fromNum;
     private TextView toNum;
-    private String base_url = "https://openexchangerates.org/api/latest.json?app_id=8a0d74b94aa44f089727cce2d2cc396a";
+    private String base_url = "http://api.fixer.io/latest?base=";
+
+    private static RequestQueue requestQueue;
 
 
 
@@ -24,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestQueue = Volley.newRequestQueue(this);
         currency = (EditText) findViewById(R.id.currency);
         fromcurr = (Spinner) findViewById(R.id.fromcurr);
         tocurr = (Spinner) findViewById(R.id.tocurr);
@@ -33,14 +51,51 @@ public class MainActivity extends AppCompatActivity {
         toNum = (TextView) findViewById(R.id.toNum);
 
 
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.currency_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         fromcurr.setAdapter(adapter);
         tocurr.setAdapter(adapter);
-         
 
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAPICall();
+            }
+        });
+    }
+
+
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    base_url + String.valueOf(fromcurr.getSelectedItem()),
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            String outputCurrency = String.valueOf(tocurr.getSelectedItem());
+                            double inputValue = Double.valueOf(currency.getText().toString());
+                            JsonParser parser = new JsonParser();
+                            JsonObject result = parser.parse(response.toString()).getAsJsonObject();
+                            JsonObject rates = result.getAsJsonObject("rates");
+                            if (rates.has(outputCurrency)) {
+                                inputValue *= rates.getAsJsonPrimitive(outputCurrency).getAsDouble();
+                            }
+                            toNum.setText(Double.toString(inputValue));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            toNum.setText(e.toString());
+            e.printStackTrace();
+        }
     }
 }
